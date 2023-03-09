185d001129a1e130a8e1259c4863ef24e43560ab
Le Nhan <Trickst6r@gmail.com>
2023-03-09 16:58:24 +0700

    Updated some files

--- sequences.py
+++ sequences.py
@@ -1,3 +1,10 @@
-name = ["Le", "Nhan", "Ricardo"]
+# name = ["Le", "Nhan", "Ricardo"]
 
-print(name[0])
+# print(name[0])
+
+
+# coordinateX = 10.0
+# coordinateY = 20.0
+
+
+# coordinate = (10.0, 20.0)
