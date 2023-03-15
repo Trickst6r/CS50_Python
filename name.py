fa29c5288d175f88b910b2fd8105943c04e3b7e5
Le Nhan <Trickst6r@gmail.com>
2023-03-15 17:48:02 +0700

    Edit files

--- name.py
+++ name.py
@@ -3,3 +3,8 @@ name = input("Name: ")
 
 # Using F-string
 print(f"Hello, {name}")
+
+# Create a chatbot that mimic the user's inputs (F-string included)
+user_said = input("Please enter your quotes: ")
+# print("Chatbot: " + user_said)
+print(f"Chatbot analyzing {user_said}")
