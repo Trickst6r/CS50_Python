f0cbf606b9febe04d6246f4f48fbbb05389c24ce
Le Nhan <Trickst6r@gmail.com>
2023-03-18 20:43:17 +0700

    Edit and add new file

---
+++ funtions.py
@@ -0,0 +1,3 @@
+# Define a function using def
+def square(x):
+    return x * x
---
+++ squares.py
@@ -0,0 +1,4 @@
+# i trong rane(10) sẽ lặp lại 10 lần từ 0 -> 9.
+# Square: Bình phương mũ 2.
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
