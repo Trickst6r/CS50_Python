aab47312c12bb7f2423527e7396177b592f66c46
Le Nhan <Trickst6r@gmail.com>
2023-03-18 16:38:18 +0700

    Edit ditionaries.py file

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,7 @@
 houses = {"Ricardo": "Slytherin", "Milos": "Dragon"}
+
+# Thêm key dict có sẵn và gán key đó vào value đã có sẵn
+houses["Hermione"] = "Slytherin"
+
+# Lấy giá trị (value) của phần tử sử dụng khoá (key) đặt trong cặp dấu []
+print(houses["Ricardo"])
