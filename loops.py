876919f7dc4ceaab1f8d66afea7c6245c3b7a54c
Le Nhan <Trickst6r@gmail.com>
2023-03-18 12:14:57 +0700

    Add loops.py file

---
+++ loops.py
@@ -0,0 +1,21 @@
+# Khi chạy chương trình, vòng lặp sẽ đi từng phần tử và
+# tới mỗi phần tử thì sẽ gọi lại i
+# for i in [0, 1, 2, 3, 4, 5]:
+#     print(i)
+
+
+# Sử dụng funtion range()
+for x in range(6):
+    print(x)
+
+# Lặp lại List:
+names = ["Harry", "Ricardo", "Milos"]
+
+for name in names:
+    print(name)
+
+# Lặp lại từng ký tự trong string:
+name = "Ricardo Milos"
+
+for character in name:
+    print(character)
