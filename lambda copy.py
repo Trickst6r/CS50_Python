3c05bdc7f714683a7e2f6083d9f568e45cc42e4f
Le Nhan <Trickst6r@gmail.com>
2023-05-05 14:28:01 +0700

    Edit file

--- lambda copy.py
+++
@@ -1,9 +0,0 @@
-people = [
-    {"name": "Harry", "house": "Ricardo"},
-    {"name": "Draco", "house": "Dancing"},
-    {"name": "Cho", "house": "Milos"},
-]
-
-
-people.sort(key=lambda person: person["name"])
-print(people)
